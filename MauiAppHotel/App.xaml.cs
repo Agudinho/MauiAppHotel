740ebfeb305020eca98b964a63ab37dd4bb1af69
Agudinho <giovanniagudinho@gmail.com>
2025-05-16 17:43:54 -0300

    Add recursos ao app

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -6,15 +6,15 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.Sobre());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
 
-            window.Width = 400;
-            window.Height = 600;
+            window.Width = 800;
+            window.Height = 800;
             return window;
         }
     }
