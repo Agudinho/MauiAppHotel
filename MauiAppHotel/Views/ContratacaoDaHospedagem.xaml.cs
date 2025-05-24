a7699ec0e9210bd793fae48244cc13174185e572
Agudinho <giovanniagudinho@gmail.com>
2025-05-24 15:52:57 -0300

    aaa

--- MauiAppHotel/Views/ContratacaoDaHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoDaHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoDaHospedagem : ContentPage
@@ -20,14 +22,28 @@ public partial class ContratacaoDaHospedagem : ContentPage
 
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
 		try
 		{
-			Navigation.PushAsync(new HospedagemContratada());
+			Hospedagem h = new Hospedagem
+			{
+				QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+				QntAdultos = Convert.ToInt32(stp_adultos.Value),
+				QntCriancas = Convert.ToInt32(stp_criancas.Value),
+				DataCheckin = dtpck_checkin.Date,
+				DataCheckout = dtpck_checkout.Date,
+			};
+
+
+
+			await Navigation.PushAsync(new HospedagemContratada()
+			{
+				BindingContext = h 
+			});
 		}catch (Exception ex)
 		{
-			DisplayAlert ("Ops", ex.Message, "OK");
+			await DisplayAlert ("Ops", ex.Message, "OK");
 		}
     }
 
